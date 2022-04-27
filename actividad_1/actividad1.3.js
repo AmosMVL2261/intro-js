204199c989cbf4d0ec0026cd83bd3f6fde4da946
Amós Vega <refi2261@gmail.com>
2022-04-26 21:30:37 -0500

    cambio en el mensaje de salida

--- actividad_1/actividad1.3.js
+++ actividad_1/actividad1.3.js
@@ -1,4 +1,4 @@
 const _PI = 3.141592;
 let ratio = 5.3;
-let result = _PI * Math.pow(ratio,2);
+let result = _PI * Math.pow(ratio,2); 
 document.write("\n<p>Resultado: &pi; &#215; ratio<sup>2</sup> = "+result+"</p>\n");
